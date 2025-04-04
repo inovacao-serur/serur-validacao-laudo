0eec9282a05da4c55e695a61339f338d02e8305e
Gabriel Barbosa <gabrieldecarvalhu@gmail.com>
2025-04-04 11:59:08 -0300

    fix: arrumando detalhes da pg de login

--- src/app/page.js
+++ src/app/page.js
@@ -9,23 +9,38 @@ export default function Login() {
   return (
     <main className="w-screen h-screen flex justify-center items-center flex-col">
       <div className="flex justify-center items-center flex-col mb-8">
-        <Image src="/docverify.png"
-        width={384}
-        height={32}
+        <Image 
+        src="/docverify.png"
+        width={350}
+        height={30}
         alt="logo doc verify"
         />
-        <h1>Acesse sua conta</h1>
+        <h1 className="pt-3 text-xl text-gray-700 font-normal">Acesse sua conta</h1>
       </div>
       <div className="w-sm h-xs border-0 border-gray-300 rounded-lg shadow-2xl px-8 pt-8 pb-11">
 
-        <div className="grid w-full max-w-sm items-center gap-1.5">
+        <div className="grid w-full max-w-sm items-center gap-1.5 relative">
           <Label htmlFor="email" className="font-normal">Email</Label>
-          <Input type="email" id="email" placeholder="seu@email.com" />
+          <Image 
+          src="/email.svg"
+          width={16}
+          height={16}
+          alt="logo email"
+          className="absolute left-3 mt-5"
+          />
+          <Input type="email" id="email" placeholder="seu@email.com" className="!text-base pl-10 py-5"/>
         </div>
 
-        <div className="grid w-full max-w-sm items-center gap-1.5 mt-6">
+        <div className="grid w-full max-w-sm items-center gap-1.5 mt-6 relative">
           <Label htmlFor="senha" className="font-normal" >Senha</Label>
-          <Input type="senha" id="senha" placeholder="••••••••" />
+          <Image 
+          src="/senha.svg"
+          width={16}
+          height={16}
+          alt="logo senha"
+          className="absolute left-3 mt-5"
+          />
+          <Input type="senha" id="senha" placeholder="••••••••" className="!text-base pl-10 py-5" />
         </div>
 
         <div className="flex justify-between items-center mt-6">
