7d953e8f06ecef9c1cb4ffc9cc9109af944970b0
Gabriel Barbosa <gabrieldecarvalhu@gmail.com>
2025-04-07 13:55:34 -0300

    desenvolvendo dashboard

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,9 +1,23 @@
 import Navbar from "@/app/components/Navbar";
+import { Button } from "@/components/ui/button"
+
 
 export default function Dashboard (){
     return(
-        <section>
-            <Navbar/>
+    <>
+        <Navbar/>
+        <section className="w-screen h-full px-3 bg-gray-50">
+            
+            <div className="w-full h-full flex justify-between items-center pt-8">
+                <h1 className="text-base font-normal">Dashboard</h1>
+                <Button>+ Nova Análise</Button>
+            </div>
+
+            <div className="w-full mt-8 py-2 pl-4 bg-white border-1 border-gray-300 rounded-md">
+                <h2>Últimas Análises</h2>
+            </div>
+
         </section>
+    </>
     )
 }
